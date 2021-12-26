b30951e24c998095fee03e3635b2b2eeb3952e28
JayantiArunkumar <jayanti.rathnavel888@gmail.com>
2021-12-26 03:01:35 -0500

    Add files via upload

---
+++ Arrays1.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+import java.util.Arrays;
+
+public class Arrays1 {
+
+	public static void main(String[] args) {
+		int[] arr1= {3,2,11,4,6,7};
+		Arrays.sort(arr1);
+		int secLargest=arr1.length-2;
+		System.out.println(arr1[secLargest]);
+		
+	}
+		
+		
+	
+	
+	}
+
+
